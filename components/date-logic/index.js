ef61d97ca16a7e828d7f02a05f2dfa2fbd5ec641
Ramesh Dangudubiyyam <ramesh.dangudubiyyam1988@outlook.com>
2024-04-11 00:58:21 +0530

    feat(date-logic): returns localized date string BREAKING CHANGE: changes the return type of the getDate function

--- components/date-logic/index.js
+++ components/date-logic/index.js
@@ -2,5 +2,6 @@ module.exports = function getDate(
     locale = 'en-US',
     options = { eekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 ) {
+    console.log('Test');
     return new Date().toLocaleDateString(locale, options);
 }
